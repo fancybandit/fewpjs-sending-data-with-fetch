18b3e84b1f0265269e50cdf08928b45d8d3cb021
Jacob Klimenko <droidedjake@gmail.com>
2021-07-03 20:26:18 -0400

    commit tree bdb24311f59c8bb5667ef0401030671394af34d4

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+
+function submitData(name, email) {
+    let url = "http://localhost:3000/users";
+
+    let formData = {
+        name: name,
+        email: email
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+
+    return fetch(url, configObj)
+        .then(response => response.json())
+        .then(function(json) {
+            const li = document.createElement("li");
+            li.innerHTML = json.id;
+    
+            const idList = document.getElementById("ids");
+            idList.appendChild(li);
+        })
+        .catch(function(error) {
+            const errorDisp = document.getElementById("caught-error");
+            errorDisp.innerHTML = error.message;
+        });
+};
